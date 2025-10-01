b530b222010e843b57ec7a18fec5d151b705cd9c
Naomi Bächler <Theera4th@gmail.com>
2025-10-01 11:34:53 +0200

    fix api connection

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://ace-tau-gules.vercel.app"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/temp")
 def get_temperature(celsius: float = Query(...)):
